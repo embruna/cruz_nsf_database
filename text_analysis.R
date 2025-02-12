2fefb8ff355407eb026578228ddddb1958e6d3b9
embruna <embruna@ufl.edu>
2025-02-12 09:56:34 -0500

    summarizing by cat

--- text_analysis.R
+++ text_analysis.R
@@ -4,6 +4,11 @@ library(stringr)
 
 cruz<-read_csv("./data_clean/cruz_data_clean.csv")
 
+
+funds_by_cat<-cruz %>% 
+  filter(status_cat=="Y") %>% 
+  summarize(amt=sum(award_amount),awards=sum(n_distinct(award_id)),grants=n_distinct(summary))
+
 career<-str_detect(cruz$summary, "career:")
 summary(career)
 
