2220cf82a7491c3384c8011adc8a8e51b656ce07
embruna <embruna@ufl.edu>
2025-02-24 09:42:01 -0500

    some quick KW analysis

--- text_analysis.R
+++ text_analysis.R
@@ -5,6 +5,53 @@ library(stringr)
 cruz<-read_csv("./data_clean/cruz_data_clean.csv")
 
 
+
+# save the grant numbers in format to search WOS --------------------------
+
+# write_csv(cruz %>% select(award_id) %>% mutate(or=" OR "), "./data_raw/search.csv")
+
+
+# prep pubs with refsplitr ------------------------------------------------
+
+
+library(refsplitr)
+
+cruz_pubs<-references_read(data = "./data_raw/wos/",
+                           dir=TRUE,
+                           include_all = TRUE)
+
+cruz_pubs<-cruz_pubs %>% mutate_all(tolower) 
+titles<-cruz_pubs %>% select(TI,DE,SO)
+write_csv(titles, "./data_clean/pub_titles.csv")
+
+titles_summary<-titles %>% 
+  select(SO) %>% 
+  group_by(SO) %>% 
+  tally() %>% 
+  arrange(desc(n))|>
+  print(n=20)  
+  
+
+
+keywords<-titles %>% 
+  select(DE) %>% 
+  mutate(kw = strsplit(DE, ";")) %>%
+  unnest(kw) %>%
+  group_by(DE) %>%
+  mutate(group_id = cur_group_id()) %>% 
+  relocate(group_id,.before=1) %>% 
+  ungroup() %>%
+  select(-DE) %>% 
+  mutate(kw=trimws(kw,"both")) %>% 
+  filter(!is.na(kw)) %>% 
+  mutate(kw=gsub("\n", " ",kw)) %>% 
+  group_by(kw) %>% 
+  tally() %>% 
+  arrange(desc(n))|>
+  print(n=30)  
+
+
+
 funds_by_cat<-cruz %>% 
   filter(status_cat=="Y") %>% 
   summarize(amt=sum(award_amount),awards=sum(n_distinct(award_id)),grants=n_distinct(summary))
